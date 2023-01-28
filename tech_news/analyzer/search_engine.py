783e3abc465db0d001ea382347c70905fb807890
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2023-01-28 20:51:29 -0300

    requisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
@@ -9,7 +10,12 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        return [(new["title"], new["url"]) for new in search_news({
+            "timestamp": {
+                "$eq": datetime.fromisoformat(date).strftime("%d/%m/%Y")}})]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
