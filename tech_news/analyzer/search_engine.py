1aff8b770885c2d505ef864ece239a6dff932892
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2023-01-28 20:40:52 -0300

    requisito 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,10 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    return [(new["title"], new["url"]) for new in search_news(
+        {"title": {"$regex": title, "$options": "i"}})]
 
 
 # Requisito 7
