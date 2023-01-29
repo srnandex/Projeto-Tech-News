9e62d1fcaad62c408410051526982739f850cb6f
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2023-01-28 22:02:07 -0300

    requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,16 @@
+from operator import itemgetter
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    result = sorted(news, key=itemgetter(
+        'comments_count', "title"), reverse=True)
+    if len(result) > 5:
+        result = result[:5]
+
+    return [(new_sorted["title"], new_sorted["url"])for new_sorted in result]
 
 
 # Requisito 11
